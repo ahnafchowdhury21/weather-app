317fb0d7c0c9a14f33f78c2e3a4c98d553c774b8
Ahnaf Chowdhury <ahnafchowdhuryaudree21@gmail.com>
2025-03-19 23:52:31 +0600

    Updated error message

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("search").addEventListener("click", (e) => {
+  const searchButton = document.getElementById("search");
+  const searchButtonMobile = document.getElementById("searchMobile");
+
+  const handleSearch = (e, inputId) => {
     e.preventDefault();
-    const city = document.getElementById("searchInput");
+    const city = document.getElementById(inputId);
     const apiKey = "152af43562b1cab90f909271334ffbdd";
     const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${apiKey}&units=metric`;
     let cardTitle = document.getElementById("title");
-    console.log(city.value);
-    if (city.value.trim() == "") {
-      document.getElementById("mainDiv").style.opacity = 0;
-    } else {
-      //   fetch Weather Data
+    const spinner = document.getElementById("spinner");
+    const mainDiv = document.getElementById("mainDiv");
 
-      fetch(weatherUrl)
-        .then((response) => {
-          if (!response.ok) {
-            document.getElementById("mainDiv").style.opacity = 1;
-            cardTitle.innerText = "City or Country not found";
-            document.querySelector(".mainInfo").innerHTML = "";
-            throw new Error("Failed to fetch the data");
-          } else {
-            document.getElementById("mainDiv").style.opacity = 1;
-            cardTitle.innerHTML = `Weather in <span id="card_city" class="text-success">${city.value
-              .charAt(0)
-              .toUpperCase()}${city.value.slice(1).toLowerCase()}</span>`;
-            return response.json();
-          }
-        })
-        .then((data) => {
-          document.querySelector(".mainInfo").innerHTML = `
+    if (city.value.trim() === "") {
+      mainDiv.classList.remove("show");
+    } else {
+      spinner.style.display = "block";
+      mainDiv.classList.remove("show");
+
+      setTimeout(() => {
+        fetch(weatherUrl)
+          .then((response) => {
+            if (!response.ok) {
+              mainDiv.classList.add("show");
+              cardTitle.innerText = "City or Country not found";
+              document.querySelector(".mainInfo").innerHTML = "";
+              throw new Error("Failed to fetch the data");
+            } else {
+              mainDiv.classList.add("show");
+              cardTitle.innerHTML = `Weather in <span id="card_city" class="text-success">${city.value
+                .charAt(0)
+                .toUpperCase()}${city.value.slice(1).toLowerCase()}</span>`;
+              return response.json();
+            }
+          })
+          .then((data) => {
+            document.querySelector(".mainInfo").innerHTML = `
+          <br>
+          <p
+      style="
+        font-family: Arial, sans-serif;
+        letter-spacing: 1px;
+        word-spacing: 2px;
+        width: 100%;
+        text-align: left;
+        font-size: 1.25rem;
+        font-weight: 600;
+      "
+    >
+      Country: ${data.sys.country}
+    </p>
         <p
       style="
         font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS',
@@ -123,17 +144,30 @@ document.addEventListener("DOMContentLoaded", () => {
       "
     >
       Wind Speed: ${data.wind.speed}m/s (${(data.wind.speed * 3.6).toFixed(
-            2
-          )}km/h)
+              2
+            )}km/h)
     </p>
       
         `;
 
-          console.log(data);
-        })
-        .catch((error) => {
-          console.log("Error:", error);
-        });
+            console.log(data);
+          })
+          .catch((error) => {
+            console.log("Error:", error);
+          })
+          .finally(() => {
+            spinner.style.display = "none";
+            mainDiv.classList.add("show");
+          });
+      }, 300);
     }
-  });
+  };
+
+  // Event listener for the navbar search box
+  searchButton.addEventListener("click", (e) => handleSearch(e, "searchInput"));
+
+  // Event listener for the mobile search box
+  searchButtonMobile.addEventListener("click", (e) =>
+    handleSearch(e, "searchInputMobile")
+  );
 });
